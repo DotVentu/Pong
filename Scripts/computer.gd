a4e7d40d88a09af2ae89c677787bca953dd42271
DURRVenture <durrdurrgamer@gmail.com>
2024-12-05 21:11:30 -0600

    fix(computer-position): corrected the position of the computer controller

--- Scripts/computer.gd
+++ Scripts/computer.gd
@@ -1,17 +1,20 @@
 extends CharacterBody2D
 
-var speed = 450
+var speed = 400
 var ball 
 
 func _ready():
-    ball = get_parent().get_node("Ball")
+	ball = get_parent().get_node("Ball")
 
 func _physics_process(delta):
-    if ball.position.y < position.y:
-        velocity.y = -1 
-    else:
-        velocity.y = 1
-    
-    velocity *= speed
+	if abs(ball.position.y - position.y) < 10:
+		return
 
-    move_and_collide(velocity * delta)
+	if ball.position.y < position.y:
+		velocity.y = -1 
+	else:
+		velocity.y = 1
+	
+	velocity *= speed
+
+	move_and_collide(velocity * delta)
