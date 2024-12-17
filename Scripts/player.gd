44c0aa729f6f664621c27c7d676f38509465fcfa
DURRVenture <durrdurrgamer@gmail.com>
2024-12-17 17:43:17 -0600

    feat(player-movement): enable player movement using the mouse
    - Added logic to control player movement based on mouse position.
    - Implemented movement clamping to restrict player within game bounds.
    - Calculated and tracked current velocity for potential future use.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,20 +2,43 @@ extends CharacterBody2D
 
 var speed = 500
 
+const TOP_LIMIT = 20
+const BOTTOM_LIMIT = 648
+const LEFT_LIMIT = 50
+const RIGHT_LIMIT = 1000
+
+var previous_position = Vector2.ZERO  
+var current_velocity = Vector2.ZERO   
+
 func _physics_process(delta):
-	velocity.y = 0
-	velocity.x = 0
+	var mousePosition = get_viewport().get_mouse_position()
+	var movement = mousePosition - position
 
-	if Input.is_action_pressed("ui_up"):
-		velocity.y = -1
-	elif Input.is_action_pressed("ui_down"):
-		velocity.y = 1
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -1
-	elif Input.is_action_pressed("ui_right"):
-		velocity.x = 1    
+	current_velocity = (position - previous_position) / delta
+	previous_position = position 
+
+	move_and_collide(movement * delta * 20)
 	
-	velocity.y *= speed
-	velocity.x *= speed
+	position.x = clamp(position.x, LEFT_LIMIT, RIGHT_LIMIT)
+	position.y = clamp(position.y, TOP_LIMIT, BOTTOM_LIMIT)
 
-	move_and_collide(velocity * delta)
+func get_current_velocity():
+	return current_velocity  
+
+
+	# velocity.y = 0
+	# velocity.x = 0
+
+	# if Input.is_action_pressed("ui_up"):
+	# 	velocity.y = -1
+	# elif Input.is_action_pressed("ui_down"):
+	# 	velocity.y = 1
+	# elif Input.is_action_pressed("ui_left"):
+	# 	velocity.x = -1
+	# elif Input.is_action_pressed("ui_right"):
+	# 	velocity.x = 1    
+	
+	# velocity.y *= speed
+	# velocity.x *= speed
+
+	# move_and_collide(velocity * delta)
